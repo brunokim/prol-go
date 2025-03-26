94aeade64b0e394ff71ef69807f15e0badaf9d18
Bruno Kim <brunokim.mc@gmail.com>
2025-03-26 01:15:58 -0300

    Implement asserta/1 and assertz/1 in terms of builtins

--- prol/lib/bootstrap.pl
+++ prol/lib/bootstrap.pl
@@ -280,3 +280,33 @@ ascii_upper(\Z).
 space(\ ).
 space(\
 ).
+
+doc(finally, we_introduce_some_standard_library_predicates).
+
+length(L, N) :-
+  length_(L, 0, N).
+
+length_([], Acc, Acc).
+length_(\.(_, T), Acc0, N) :-
+  is(Acc, \+(Acc0, 1)),
+  length_(T, Acc, N).
+
+append([], T, T).
+append(\.(H, L0), L1, \.(H, L2)) :-
+  append(L0, L1, L2).
+
+indicator(struct(Name, Args), indicator(Name, NumArgs)) :-
+  length(Args, NumArgs).
+
+asserta(Clause) :-
+  \=(Clause, clause(Head, _)),
+  indicator(Head, Ind),
+  get_predicate(Ind, Clauses0),
+  put_predicate(Ind, \.(Clause, Clauses0)).
+
+assertz(Clause) :-
+  \=(Clause, clause(Head, _)),
+  indicator(Head, Ind),
+  get_predicate(Ind, Clauses0),
+  append(Clauses0, \.(Clause, []), Clauses),
+  put_predicate(Ind, Clauses).
